4640d4d477e6602036439ab5cffe931e79c2b71e
Andrei <b1naryth1ef@gmail.com>
2016-10-09 02:10:02 -0500

    Move flask imports inside of flashy function to avoid import issues

--- holster/util.py
+++ holster/util.py
@@ -1,11 +1,10 @@
-from flask import flash, redirect
-
 
 def flashy(msg, etype="danger", path="/"):
     """
     Flashes a message and redirects a user. Returns a flask Response that can
     be returned to the user.
     """
+    from flask import flash, redirect
     flash(msg, etype)
     return redirect(path)
 
