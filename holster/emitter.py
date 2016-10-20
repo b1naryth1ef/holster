c2154fe2b295446752f02941a4912fe891dfa1e9
Andrei <b1naryth1ef@gmail.com>
2016-10-20 03:08:31 -0500

    Various emitter improvements

--- holster/emitter.py
+++ holster/emitter.py
@@ -9,6 +9,10 @@ Priority = Enum(
 )
 
 
+class Stop(Exception):
+    pass
+
+
 class Event(object):
     def __init__(self, parent, data):
         self.parent = parent
@@ -21,23 +25,30 @@ class Event(object):
 
 
 class EmitterSubscription(object):
-    def __init__(self, events, prio, func):
+    def __init__(self, events, func, priority=Priority.NONE, conditional=None):
         self.events = events
-        self.prio = prio
         self.func = func
+        self.priority = priority
+        self.conditional = conditional
         self.emitter = None
+
+    def __call__(self, *args, **kwargs):
+        if callable(self.conditional):
+            if not self.conditional(*args, **kwargs):
+                return
+        return self.func(*args, **kwargs)
 
     def add(self, emitter):
         self.emitter = emitter
         for event in self.events:
-            emitter.event_handlers[self.prio][event].append(self)
+            emitter.event_handlers[self.priority][event].append(self)
         return self
 
     def remove(self, emitter=None):
         emitter = emitter or self.emitter
 
         for event in self.events:
-            emitter.event_handlers[self.prio][event].remove(self)
+            emitter.event_handlers[self.priority][event].remove(self)
 
 
 class Emitter(object):
@@ -48,18 +59,35 @@ class Emitter(object):
         }
 
     def _call(self, func, args, kwargs):
-        if self.wrapper:
-            self.wrapper(func, *args, **kwargs)
-        else:
+        try:
             func(*args, **kwargs)
+        except Stop:
+            pass
 
     def emit(self, name, *args, **kwargs):
         for prio in [Priority.BEFORE, Priority.NONE, Priority.AFTER]:
-            for listener in self.event_handlers[prio].get(name, []):
-                self._call(listener.func, args, kwargs)
+            for listener in self.event_handlers[prio].get(name, []) + self.event_handlers[prio].get('', []):
+                if self.wrapper:
+                    self.wrapper(self._call, listener, args, kwargs)
+                else:
+                    self._call(listener, args, kwargs)
 
     def on(self, *args, **kwargs):
-        return EmitterSubscription(args[:-1], kwargs.pop('priority', Priority.NONE), args[-1]).add(self)
+        return EmitterSubscription(args[:-1], args[-1], **kwargs).add(self)
+
+    def once(self, *args, **kwargs):
+        from gevent.event import AsyncResult
+
+        result = AsyncResult()
+        li = None
+
+        def _f(e):
+            result.set(e)
+            li.remove()
+
+        li = self.on(*args + (_f, ))
+
+        return result.wait(kwargs.pop('timeout', None))
 
     def wait(self, *args, **kwargs):
         from gevent.event import AsyncResult
